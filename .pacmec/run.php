b904f2b5a0f9a886e751bd892bc8b30532182249
FelipheGomez <feliphegomez@gmail.com>
2021-04-14 21:22:43 -0500

    test

--- .pacmec/run.php
+++ .pacmec/run.php
@@ -16,6 +16,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+exec("git pull origin master");
 
 if (!defined('PACMEC_PATH')) define('PACMEC_PATH', __DIR__ . '/');
 
