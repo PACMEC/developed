83591362ed7d4474dd0af4877c18ff618123cad2
FelipheGomez <feliphegomez@gmail.com>
2021-04-14 21:09:22 -0500

    Enable Errors

--- .pacmec/run.php
+++ .pacmec/run.php
@@ -8,6 +8,14 @@
  * @license    license.txt
  * @version    0.0.1
  */
+/**
+*
+* DISPLAY ERRORS ENABLED
+*
+**/
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 if (!defined('PACMEC_PATH')) define('PACMEC_PATH', __DIR__ . '/');
 
